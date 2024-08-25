1d4499c562a1f02602b3d4b9862f10e4b75bb844
Amanda <phototix@hotmail.com>
2024-08-25 16:56:05 +0800

    Test template.

---
+++ includes/htmlstart.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8" />
+<title>EzChat | WhatsApp Customer Service Chat Application</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="EzChat - WhatsApp Customer Service Chat Application" name="description" />
+<meta name="keywords" content="WhatsApp Customer Service Chat Application"/>
+<meta content="EzChat" name="author" />
+<!-- App favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.ico" id="tabIcon">
+
+
+<!-- Bootstrap Css -->
+<link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<!-- Icons Css -->
+<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
