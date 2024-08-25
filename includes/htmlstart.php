510cf0b5e968a5676af0ee03dc9a38c627aa1d88
Amanda <phototix@hotmail.com>
2024-08-25 17:10:29 +0800

    Load all assets from root /

--- includes/htmlstart.php
+++ includes/htmlstart.php
@@ -10,15 +10,15 @@
 <meta name="keywords" content="WhatsApp Customer Service Chat Application"/>
 <meta content="EzChat" name="author" />
 <!-- App favicon -->
-<link rel="shortcut icon" href="assets/images/favicon.ico" id="tabIcon">
+<link rel="shortcut icon" href="/assets/images/favicon.ico" id="tabIcon">
 
 
 <!-- Bootstrap Css -->
-<link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+<link href="/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
 
 
 </head>
