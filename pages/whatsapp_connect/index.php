e4f2031618fb266f3acb5481ea9e01b7a82f0943
Amanda <phototix@hotmail.com>
2025-02-24 19:29:31 +0800

    Remove encode

--- pages/whatsapp_connect/index.php
+++ pages/whatsapp_connect/index.php
@@ -99,7 +99,7 @@ if ($status === 'WORKING') {
                             Scan this QR Code to Connect WhatsApp
                           </div>
                           <div class="card-body">
-                            <img src="data:image/png;<?= base64_encode($qrCode) ?>" alt="QR Code" class="img-fit">
+                            <img src="data:image/png;base64,<?=$qrCode?>" alt="QR Code" class="img-fit">
                             <script>
                                 setTimeout(function() {
                                     window.location.href = "";
