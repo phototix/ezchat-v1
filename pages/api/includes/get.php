fd0de124a24bfed9e1fffb70f3f21bcaf2a84adb
Amanda <phototix@hotmail.com>
2025-02-24 19:57:36 +0800

    Add API interface.

--- pages/api/includes/get.php
+++ pages/api/includes/get.php
@@ -1,1 +1,13 @@
-get
+<?php
+// Example data to be returned as JSON
+$responseData = [
+    "status" => "success",
+    "message" => "Data retrieved successfully",
+    "data" => [
+        "id" => 123,
+        "name" => "Example Item",
+        "price" => 49.99
+    ]
+];
+// Return the JSON-encoded response
+echo json_encode($responseData);
--- pages/api/index.php
+++ pages/api/index.php
@@ -1,8 +1,14 @@
 <?php
+// Set the Content-Type header to application/json
+header('Content-Type: application/json');
+
 if($cate==""||$cate=="list"){
 	$pageURL="includes/main.php";
 }else{
 	$pageURL="includes/".$cate.".php";
 }
 include($pageURL);
+
+// Exit to ensure no additional output is sent
+exit;
 ?>
