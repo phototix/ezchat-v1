6a5d2755ea8fa2b6fbbbc97f5147e20fedccde7f
Amanda <phototix@hotmail.com>
2025-02-24 22:26:22 +0800

    return Json after process

--- pages/api/methods/post.php
+++ pages/api/methods/post.php
@@ -1,9 +1,9 @@
 <?php
 if ($cate <> "") {
-	echo "test";
     $pageURL = 'pages/api/includes/'.$cate.'.php';
     // Check if the file exists before including it
     if (file_exists($pageURL)) {
+    	echo "test";
         include($pageURL);
     } else {
         $pageURL = 'pages/api/includes/main.php';
