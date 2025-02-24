4eeec0e211fe30a526f24048e0c96a695384b5f0
Amanda <phototix@hotmail.com>
2025-02-24 20:12:18 +0800

    Add API interface.

--- pages/api/methods/delete.php
+++ pages/api/methods/delete.php
@@ -1,14 +1,14 @@
 <?php
 if ($cate <> "") {
-    $pageURL = 'includes/'.$cate.'.php';
+    $pageURL = 'pages/api/includes/'.$cate.'.php';
     // Check if the file exists before including it
     if (file_exists($pageURL)) {
         include($pageURL);
     } else {
-        $pageURL = 'includes/main.php';
+        $pageURL = 'pages/api/includes/main.php';
         include($pageURL);
     }
 } else {
-    $pageURL = 'includes/main.php';
+    $pageURL = 'pages/api/includes/main.php';
     include($pageURL);
 }
