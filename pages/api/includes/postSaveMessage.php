56620e9bbf0f4e345f3c40ced2d426c90882f282
Amanda <phototix@hotmail.com>
2025-02-24 22:29:08 +0800

    return Json after process

--- pages/api/includes/postSaveMessage.php
+++ pages/api/includes/postSaveMessage.php
@@ -12,36 +12,36 @@ http_response_code(200); // OK
 echo json_encode(["status" => "success", "message" => "Data successfully processed"]);
 
 // Insert into webhook_events table
-$stmt = $mysqli->prepare("INSERT INTO webhook_events (id, event, session, engine, environment_version, environment_engine, environment_tier, environment_browser) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt = $pdo->prepare("INSERT INTO webhook_events (id, event, session, engine, environment_version, environment_engine, environment_tier, environment_browser) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("ssssssss", $data['id'], $data['event'], $data['session'], $data['engine'], $data['environment']['version'], $data['environment']['engine'], $data['environment']['tier'], $data['environment']['browser']);
 $stmt->execute();
 $stmt->close();
 
 // Insert into webhook_users table
-$stmt = $mysqli->prepare("INSERT INTO webhook_users (id, pushName) VALUES (?, ?)");
+$stmt = $pdo->prepare("INSERT INTO webhook_users (id, pushName) VALUES (?, ?)");
 $stmt->bind_param("ss", $data['me']['id'], $data['me']['pushName']);
 $stmt->execute();
 $stmt->close();
 
 // Insert into webhook_payloads table
 $payload = $data['payload'];
-$stmt = $mysqli->prepare("INSERT INTO webhook_payloads (id, event_id, timestamp, from_user, to_user, body, hasMedia, ack, ackName) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt = $pdo->prepare("INSERT INTO webhook_payloads (id, event_id, timestamp, from_user, to_user, body, hasMedia, ack, ackName) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("ssssssiii", $payload['id'], $data['id'], $payload['timestamp'], $payload['from'], $payload['to'], $payload['body'], $payload['hasMedia'], $payload['ack'], $payload['ackName']);
 $stmt->execute();
 $stmt->close();
 
 // Insert into webhook_message_data table
 $msg_data = $payload['_data'];
-$stmt = $mysqli->prepare("INSERT INTO webhook_message_data (id, payload_id, viewed, message_body, message_type, timestamp, notifyName, from_user, to_user, ack, invis, isNewMsg, star, kicNotified, recvFresh, isFromTemplate, labels) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt = $pdo->prepare("INSERT INTO webhook_message_data (id, payload_id, viewed, message_body, message_type, timestamp, notifyName, from_user, to_user, ack, invis, isNewMsg, star, kicNotified, recvFresh, isFromTemplate, labels) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("ssssssssiiiiiiii", $msg_data['id'], $payload['id'], $msg_data['viewed'], $msg_data['body'], $msg_data['type'], $msg_data['t'], $msg_data['notifyName'], $msg_data['from'], $msg_data['to'], $msg_data['ack'], $msg_data['invis'], $msg_data['isNewMsg'], $msg_data['star'], $msg_data['kicNotified'], $msg_data['recvFresh'], $msg_data['isFromTemplate'], implode(",", $msg_data['labels']));
 $stmt->execute();
 $stmt->close();
 
 // Insert webhook_message_secrets table
-$stmt = $mysqli->prepare("INSERT INTO webhook_message_secrets (message_data_id, secret) VALUES (?, ?)");
+$stmt = $pdo->prepare("INSERT INTO webhook_message_secrets (message_data_id, secret) VALUES (?, ?)");
 $stmt->bind_param("sb", $msg_data['id'], json_encode($msg_data['messageSecret']));
 $stmt->execute();
 $stmt->close();
 
-$mysqli->close();
+$pdo->close();
 ?>
