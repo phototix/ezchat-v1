39434de8ee27e4b86e35488253609f3687e94c21
Amanda <phototix@hotmail.com>
2025-02-24 22:12:35 +0800

    return Json after process

--- developer/database/tables_backup.sql
+++ developer/database/tables_backup.sql
@@ -54,6 +54,6 @@ CREATE TABLE webhook_message_data (
 CREATE TABLE webhook_message_secrets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     message_data_id VARCHAR(255),
-    secret BYTEA,
+    secret TEXT,
     FOREIGN KEY (message_data_id) REFERENCES webhook_message_data(id)
 );
